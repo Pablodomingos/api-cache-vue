f99e52e3cd3fb0694a479208816365039f549895
Pablodomingos <pablodomingos1700@gmail.com>
2023-05-26 23:23:28 -0300

    Feat: consume api and show in view.

--- src/views/course/CoursesView.vue
+++ src/views/course/CoursesView.vue
@@ -1,3 +1,43 @@
 <template>
     <h1>Listagem de cursos</h1>
+
+    <div v-if="loading">
+        <p>Carregando ...</p>
+    </div>
+
+    <ul>
+        <li v-for="course in courses" :key="course.id">
+            {{ course.title }}
+            <div v-for="(module, index) in course.modules" :key="module.id">
+                {{ `${index+1} - ${module.name}` }}<br>
+            </div>
+        </li>
+    </ul>
 </template>
+
+<script>
+import { onMounted, ref } from 'vue'
+import CourseService from '@/services/CourseService'
+
+export default {
+    name: 'CoursesView',
+
+    setup () {
+        const courses = ref([])
+        const loading = ref(false)
+
+        onMounted(async () => {
+            loading.value = true
+            CourseService.getCourses()
+                .then(response => courses.value = response.data.data)
+                .catch(error => console.log(error))
+                .finally(() => loading.value = false)
+        })
+
+        return {
+            loading,
+            courses
+        }
+    }
+}
+</script>
