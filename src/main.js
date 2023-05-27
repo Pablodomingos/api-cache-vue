968ec81be2e223ab938543d3d034b2f1fc5bebb0
Pablodomingos <pablodomingos1700@gmail.com>
2023-05-26 23:22:01 -0300

    Feat: import axios and url of the api.

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import { createApp } from 'vue'
 import App from '@/App.vue'
 import router from '@/router'
+import axios from 'axios'
+import { API_URL } from '@/config/api'
+
+axios.defaults.baseURL = API_URL
 
 createApp(App)
     .use(router)
