cd80c5036586a3ac2fd358bbed64b13a780c3f7a
Pablodomingos <pablodomingos1700@gmail.com>
2023-05-29 20:38:51 -0300

    Feat: added route for create course.

--- src/router/index.js
+++ src/router/index.js
@@ -1,17 +1,21 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '@/views/HomeView.vue'
 import CoursesView from '@/views/course/CoursesView.vue'
+import CourseCreate from '@/views/course/CourseCreate.vue'
 
 const routes = [
   {
     path: '/',
     name: 'home',
     component: HomeView
-  },
-  {
+  }, {
     path: '/courses',
     name: 'courses.index',
     component: CoursesView
+  }, {
+    path: '/course/',
+    name: 'course.create',
+    component: CourseCreate
   }
 ]
 
