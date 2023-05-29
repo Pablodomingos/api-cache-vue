6919a19edd63a4738287abe6b73674b9fc075429
Pablodomingos <pablodomingos1700@gmail.com>
2023-05-29 20:39:16 -0300

    Feat: added method for create course.

--- src/services/CourseService.js
+++ src/services/CourseService.js
@@ -4,4 +4,8 @@ export default class CourseService {
     static async getCourses() {
         return axios.get("/courses");
     }
+
+    static async createCourse(params) {
+        return axios.post("/course", params);
+    }
 }
